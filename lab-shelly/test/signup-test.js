a865155d0c20a7ad5c5132b5301bd936fbb021f5
Shelly Tang <shellytang@gmail.com>
2017-06-06 10:26:19 -0700

    sign up test passing

--- lab-shelly/test/signup-test.js
+++ lab-shelly/test/signup-test.js
@@ -1,58 +1,49 @@
-// 'use strict';
-//
-// describe('testing auth signup form', function() {
-//
-//   beforeEach(() => {
-//     angular.mock.module('cfgram');
-//     angular.mock.inject(($rootScope, $componentController, $httpBackend) => {
-//       this.$rootScope = $rootScope;
-//       this.signupCtrl = $componentController('signup');
-//       this.$httpBackend = $httpBackend;
-//       // this.signupCtrl.authService = authService;
-//       this.signupCtrl.$onInit();
-//     });
-//   });
-//
-//   afterEach(() => {
-//     this.$httpBackend.verifyNoOutstandingExpectation();
-//     this.$httpBackend.verifyNoOutstandingRequest();
-//   });
-//
-//   afterEach(() => this.$rootScope.$apply());
-//
-//   it('should send data to the server', () => {
-//     let expectUrl = 'http://localhost:3000/api/signup';
-//     let expectBody = {
-//       username: 'testingusername',
-//       password: 'password123',
-//       email: 'testemail@email.com',
-//     };
-//     let expectHeaders = {
-//       'Content-Type': 'application/json',
-//       Accept: 'application/json',
-//     };
-// //make the request
-//     this.signupCtrl.user = {
-//       username: 'testingusername',
-//       password: 'password123',
-//       email: 'testemail@email.com',
-//     };
-//
-//     this.$httpBackend.expectPOST(expectUrl, expectBody, expectHeaders)
-//     .respond(200, 'exampletoken1234');
-//
-//     console.log('what?', this.signupCtrl);
-//     console.log('what?', this.signupCtrl.authService);
-//     this.signupCtrl.signup();
-//     //flush backend
-//     this.$httpBackend.flush();
-//
-//     // describe('testing the test', function(){
-//     //   it('should pass this test', () => {
-//     //     expect(true).toEqual(true);
-//     //   });
-//     // });
-//
-//   });
-//
-// });
+'use strict';
+
+describe('Sign up Component', function() {
+
+  beforeEach(() => {
+    angular.mock.module('cfgram');
+    angular.mock.inject(($rootScope, $httpBackend, $window, $componentController) => {
+      this.$rootScope = $rootScope;
+      this.$httpBackend = $httpBackend;
+      this.$window = $window;
+      this.signupCtrl = $componentController('signup');
+    });
+  });
+
+  afterEach(() => {
+    this.$window.localStorage.removeItem('token');
+    this.$httpBackend.flush();
+    this.$rootScope.$apply();
+  });
+
+  it('should send data to the server', () => {
+
+    let expectUrl = 'http://localhost:3000/api/signup';
+    let expectBody = {
+      username: 'testingusername',
+      password: 'password123',
+      email: 'testemail@email.com',
+    };
+
+    let expectHeaders = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    };
+//make the request
+    this.signupCtrl.user = {
+      username: 'testingusername',
+      password: 'password123',
+      email: 'testemail@email.com',
+    };
+
+    this.signupCtrl.$onInit();
+    this.$httpBackend.expectPOST(expectUrl, expectBody, expectHeaders)
+    .respond(200, 'exampletoken1234');
+    let runSignup = () => {
+      this.signupCtrl.signup(this.signupCtrl.user);
+    };
+    expect(runSignup).not.toThrow();
+  });
+});
